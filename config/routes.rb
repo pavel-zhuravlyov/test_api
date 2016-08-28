7c5edc1bf4db528a47d9541db14865aab9210484
pavel-zhuravlyov <pavlozhuravlyov@gmail.com>
2016-08-28 03:03:49 +0300

    change config for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # before_filter :authenticate_user!
-
   post 'analyser/analyse'
   post 'analyser/correlation'
 
   mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
