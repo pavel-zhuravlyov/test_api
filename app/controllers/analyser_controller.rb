c64e01f2a0f8fe2e7add75afce58d06a777e4aae
pavel-zhuravlyov <pavlozhuravlyov@gmail.com>
2016-08-28 18:14:06 +0300

    fix analyser controller

--- app/controllers/analyser_controller.rb
+++ app/controllers/analyser_controller.rb
@@ -1,11 +1,21 @@
 class AnalyserController < ApplicationController
   before_filter :authenticate_user!
 
-  def
-    render json: Analyser.new.analyse(params[:dataset])
+  def analyse
+    result = Analyser.new.analyse(params[:dataset])
+    if result
+      render json: result
+    else
+      render json: { :errors => ['Invalid data'] }, :status => 422
+    end
   end
 
   def correlation
-    render json: { correlation: Analyser.new.correlation(params[:first_dataset], params[:second_dataset]) }
+    result = Analyser.new.correlation(params[:first_dataset], params[:second_dataset])
+    if result
+      render json: { correlation: result }
+    else
+      render json: { errors: ['Invalid data'] }, status: 422
+    end
   end
 end
